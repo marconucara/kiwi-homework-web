b9ceb782e303c2f4802b087ca820b0760cb0e161
Marco Nucara <marco.nucara@gmail.com>
2018-11-06 21:56:53 +0100

    Add styled-components, remove css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './globalStyle';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 12s ease-in-out;
+  height: 80px;
+  &:hover {
+    transform: scale3d(1.1, 1.1, 1);
+  }
+`;
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 12rem;
+  padding: 1rem;
+  color: white;
+`;
+
+const AppTitle = styled.h1`
+  font-weight: 900;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper>
+        <GlobalStyle />
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppTitle>
+            Coming soon...
+          </AppTitle>
+        </AppHeader>
+      </AppWrapper>
     );
   }
 }
